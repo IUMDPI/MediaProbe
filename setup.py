ca893631d05c37d2a2bd332a181144db27c3aa75
Brian Wheeler <bdwheele@indiana.edu>
2019-06-10 10:12:43 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='mediaprobe',
+      version='0.9',
+      url="https://github.com/IUMDPI/MediaProbe",
+      maintainer="Brian Wheeler",
+      maintainer_email="bdwheele@indiana.edu")
+      
