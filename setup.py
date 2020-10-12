e93f0eb70854124c9287137f041cc1c209a38073
Brian Wheeler <bdwheele@indiana.edu>
2020-10-12 13:32:41 -0400

    use underscore in name

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 from distutils.core import setup
-setup(name='mediaprobe',
+setup(name='media_probe',
       version='0.9',
       url="https://github.com/IUMDPI/MediaProbe",
       maintainer="Brian Wheeler",
