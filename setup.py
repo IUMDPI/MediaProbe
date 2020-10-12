0232877f5827c7b38e0b6879c9b40bed43784034
Brian Wheeler <bdwheele@indiana.edu>
2020-10-12 13:37:40 -0400

    convert to setuptools

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(name='media_probe',
       version='0.9',
       url="https://github.com/IUMDPI/MediaProbe",
       maintainer="Brian Wheeler",
-      maintainer_email="bdwheele@indiana.edu")
-      
+      maintainer_email="bdwheele@indiana.edu",
+      packages=find_packages()
+)
